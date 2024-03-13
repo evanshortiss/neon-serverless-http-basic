43e1461d27ed5ee90bb3e8b64826b747b151071b
Evan Shortiss <evanshortiss@gmail.com>
2024-03-13 15:44:50 -0700

    cursed testing code

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-const { neon } = require('@neondatabase/serverless')
-const sql = neon(process.env.DATABASE_URL)
 
-sql('SELECT version()')
-  .then(result => console.log(result))
-  .catch(err => console.log(err))
+function httpVersion () {
+  const { neon } = require('@neondatabase/serverless')
+  const sql = neon(process.env.DATABASE_URL)
+  const time = Date.now()
+  
+  sql('SELECT version()')
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+    .finally(() => console.log(`Total Time (HTTP): ${Date.now() - time}ms`))
+}
+
+function poolVersion () {
+  const { Pool, neonConfig } = require('@neondatabase/serverless')
+  const ws = require('ws').WebSocket
+
+  neonConfig.webSocketConstructor = ws;
+  
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const time = Date.now()
+
+  pool.connect()
+    .then(async (client) => {
+      const result = await client.query('SELECT version()')
+      console.log(result.rows)
+      client.end()
+      return result
+    })
+    .catch(err => console.log(err))
+    .finally(() => {
+      console.log(`Total Time (Pool): ${Date.now() - time}ms`)
+      pool.end()
+    })
+}
+
+poolVersion()
+httpVersion()
